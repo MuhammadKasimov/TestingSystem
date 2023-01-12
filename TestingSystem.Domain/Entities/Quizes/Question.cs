854012a576dcd5d03a7d61ccdf22c6195ca51ac5
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-01-12 16:41:13 +0500

    Domain: added QuizResult and Course Entities

--- TestingSystem.Domain/Entities/Quizes/Question.cs
+++ TestingSystem.Domain/Entities/Quizes/Question.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TestingSystem.Domain.Commons;
-using TestingSystem.Domain.Entities.Quizes;
 
-namespace TestingSystem.Domain.Entities.Questions
+namespace TestingSystem.Domain.Entities.Quizes
 {
     public class Question : Auditable
     {
         public int NumberOfQuestions { get; set; }
-        
+
         public int QuizId { get; set; }
         public Quiz Quiz { get; set; }
 
