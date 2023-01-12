6e813eddfa5e0e80a84c3b4f0c357bd3e0278df6
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-01-12 16:14:53 +0500

    Domain: Added User Entity

---
+++ TestingSystem.Domain/Enums/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Domain.Enums
+{
+    public enum UserRole
+    {
+        Admin,
+        Teacher,
+        Student
+    }
+}
