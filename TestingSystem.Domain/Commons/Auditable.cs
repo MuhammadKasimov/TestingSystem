29c4aa691a35e25bf15b25de5f83d851359415fc
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-01-12 15:45:30 +0500

    DOMAIN: added Auditable

---
+++ TestingSystem.Domain/Commons/Auditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Domain.Commons
+{
+    public class Auditable
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
