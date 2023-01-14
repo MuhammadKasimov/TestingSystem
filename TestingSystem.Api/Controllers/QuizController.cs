951ffaad4fb6765c28f674d93d9f1fa259e818c5
Temurkhann <109779514+Temurkhann@users.noreply.github.com>
2023-01-14 19:48:05 +0500

    Quiz Controller completed

---
+++ TestingSystem.Api/Controllers/QuizController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestingSystem.Api.Helpers;
+using TestingSystem.Domain.Configurations;
+using TestingSystem.Service.DTOs.Quizes;
+using TestingSystem.Service.Interfaces.Quizes;
+
+namespace TestingSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        private readonly IQuizService quizService;
+
+        public QuizController(IQuizService quizService)
+        {
+            this.quizService = quizService;
+        }
+
+        [HttpPost, Authorize(Roles = CustomRoles.ADMIN_ROLE)]
+        public async ValueTask<IActionResult> CreateAsync(QuizForCreationDTO quizForCreationDTO)
+        {
+            return Ok(await quizService.CreateAsync(quizForCreationDTO));
+        }
+
+        [HttpPut, Authorize(Roles = CustomRoles.ADMIN_ROLE)]
+        public async ValueTask<IActionResult> UpdateAsync(int id, QuizForCreationDTO quizForUpdateDTO)
+            => Ok(await quizService.UpdateAsync(id, quizForUpdateDTO));
+
+        [HttpGet, Authorize(Roles = CustomRoles.ALL_ROLES)]
+        public async ValueTask<IActionResult> GetAll([FromQuery] PaginationParams @params)
+           => Ok(await quizService.GetAllAsync(@params));
+
+
+        [HttpGet("{id}/Admin"), Authorize(Roles = CustomRoles.ALL_ROLES)]
+        public async ValueTask<IActionResult> GetAsync([FromRoute] int id)
+           => Ok(await quizService.GetAsync(u => u.Id == id));
+
+        [HttpDelete, Authorize(Roles = CustomRoles.ADMIN_ROLE)]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+            => Ok(await quizService.DeleteAsync(id));
+    }
+}
