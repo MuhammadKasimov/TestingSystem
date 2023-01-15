d31b2648bbda1e3ad4627240fda40d54a4ddb359
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-01-15 14:17:15 +0500

    SERVICE: AnswerService.cs completed

---
+++ TestingSystem.Service/Interfaces/Quizes/IAnswerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Domain.Configurations;
+using TestingSystem.Domain.Entities.Quizes;
+using TestingSystem.Service.DTOs.Quizes;
+
+namespace TestingSystem.Service.Interfaces.Quizes
+{
+    public interface IAnswerService
+    {
+        ValueTask<AnswerForViewDTO> CreateAsync(AnswerForCreationDTO answerForCreationDTO);
+
+        ValueTask<AnswerForViewDTO> UpdateAsync(int id, AnswerForCreationDTO answerForCreationDTO);
+
+        ValueTask<bool> DeleteAsync(int id);
+
+        ValueTask<IEnumerable<AnswerForViewDTO>> GetAllAsync(
+            PaginationParams @params,
+            Expression<Func<Answer, bool>> expression = null);
+
+        ValueTask<AnswerForViewDTO> GetAsync(Expression<Func<Answer, bool>> expression);
+
+    }
+}
