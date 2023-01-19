0b5d613aadfae9f5d3719a819095e5b90997bdd1
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-01-19 18:41:08 +0500

    Services: QuizResultService SolvedQuestions Included

--- TestingSystem.Service/DTOs/Quizes/QuizResultForViewDTO.cs
+++ TestingSystem.Service/DTOs/Quizes/QuizResultForViewDTO.cs
@@ -9,8 +9,8 @@ namespace TestingSystem.Service.DTOs.Quizes
     {
         public int Id { get; set; }
         public int CorrectAnswers { get; set; }
-        public Quiz Quiz { get; set; }
-        public User User { get; set; }
-        public ICollection<SolvedQuestion> SolvedQuestions { get; set; }
+        public QuizForViewDTO Quiz { get; set; }
+        public UserForViewDTO User { get; set; }
+        public ICollection<SolvedQuestionForViewDTO> SolvedQuestions { get; set; }
     }
 }
